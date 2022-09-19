918143d2d00d25af83e27ae9f5b8ada9593924e5
Roman Chechyotkin <romanchechyotkin@gmail.com>
2022-09-19 18:34:13 +0300

    ThemeSwitcher widget added with custom Button in shared

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,27 @@
+import React, {ButtonHTMLAttributes} from "react";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Button.module.scss'
+
+export enum ThemeButton {
+    CLEAR='clear',
+
+}
+
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    className?: string;
+    theme?: ThemeButton;
+}
+
+export const Button: React.FC<ButtonProps> = (props) => {
+    const {children, className, theme, ...otherProps} = props
+
+    return (
+        <button
+            className={classNames(cls.Button, {}, [className, cls[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss'
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 
 interface NavbarProps {
     className?: string;
@@ -10,6 +11,7 @@ interface NavbarProps {
 export const Navbar: React.FC<NavbarProps> = ({className}) => {
     return (
         <nav className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher/>
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>main</AppLink>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>about</AppLink>
