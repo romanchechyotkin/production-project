812988bf57b9e6bf4ceb31c465e3d1bdd3314fbf
Roman Chechyotkin <romanchechyotkin@gmail.com>
2022-10-03 20:30:00 +0300

    error handler added

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
-import cls from './Navbar.module.scss';
+import React from "react";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {useTranslation} from "react-i18next";
+import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar: React.FC<NavbarProps> = ({ className }) => {
-    const { t } = useTranslation('navbar');
+export const Navbar: React.FC<NavbarProps> = ({className}) => {
+    const {t} = useTranslation("navbar");
 
     return (
         <nav className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/">{t('main')}</AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('about')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/">{t("main")}</AppLink>
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t("about")}</AppLink>
             </div>
         </nav>
     );
